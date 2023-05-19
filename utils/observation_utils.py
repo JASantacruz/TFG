a203f8adf55dd5389466ff5bdda5ed6bed7a9ffc
JASantacruz <joseantonio.santacruz@alu.uclm.es>
2023-05-19 16:20:31 +0200

    Update observation utils file

--- utils/observation_utils.py
+++ utils/observation_utils.py
@@ -1,4 +1,24 @@
+import json
 
 def GetObservationTotal(observation):
     total = observation.json()['total']
     return total
+
+def GetObservationDisplay(observation):
+    display = observation['resource']['code']['coding'][0]['display']
+    return display or ''
+
+def GetObservationValue(observation):
+    value = observation['resource']['valueQuantity']['value']
+    return value or ''
+
+def GetObservationUnit(observation):
+    unit = observation['resource']['valueQuantity']['unit']
+    return unit or ''
+
+def ParseResponse(response):
+    observations = ''
+    for i in response['entry']:
+        observations += 'Observation - '+ str(GetObservationDisplay(i)) + ' ' + str(GetObservationValue(i)) + str(GetObservationUnit(i)) + '\n'
+
+    return observations 
